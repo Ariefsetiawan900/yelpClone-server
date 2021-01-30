05da165d9434755103e0a00bae7dc36b66cdaa72
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-01-30 13:44:16 +0700

    create route get a restaurant & post create

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const express = require("express");
 const app = express();
 const Port = process.env.PORT || 3009;
 
-app.get('/getRestaurants',(req,res)=>{
-    res.json({
-        status:"success",
-        restaurants:"mcdonald"
-    })
-})
+// get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurants: ["Starbuck", "Mcdonald", "KFC"],
+    },
+  });
+});
+
+// Get a restaurant
+app.get("/api/v1/restaurants/:restaurantsid", (req, res) => {
+  console.log(req.params);
+});
+
+// Create a Restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req);
+});
 
 app.listen(Port, () => {
   console.log(`server is up and listening on port ${Port}`);
