a0fbc2559b4e1c6105dd01ec74a4e970477fc9e2
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-02-01 17:30:37 +0700

    create Table reviews

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE yelpClone;
+-- Main Table 
 
 CREATE TABLE restaurants (
     id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -8,3 +9,15 @@ CREATE TABLE restaurants (
 );
 
 INSERT INTO restaurants (name, location, price_range) VALUES ('sate', 'indo', 4);
+SELECT * FROM reviews
+DROP TABLE *
+
+-- Second Table
+
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurants_id  BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check(rating >= 1 and rating <= 5)
+);
